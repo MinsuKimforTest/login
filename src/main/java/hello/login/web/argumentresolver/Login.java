e469fdd05be09f5ed3e6b288f1c597b9a7e15fb5
김민수 <kimms2@lionplus.co.kr>
2023-11-09 09:52:30 +0900

    231109

---
+++ src/main/java/hello/login/web/argumentresolver/Login.java
@@ -0,0 +1,11 @@
+package hello.login.web.argumentresolver;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)  //동작할때까지 Annotation이 남아있어야하기때문에
+public @interface Login {
+}
